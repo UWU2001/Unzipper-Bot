ce4148a06d34793e0d41090c5dcc8cd531ebf43a
frankenstein <157962267+UWU2001@users.noreply.github.com>
2025-08-19 21:49:04 +0300

    Update __init__.py

--- unzipper/__init__.py
+++ unzipper/__init__.py
@@ -7,12 +7,8 @@ from .client.caching import update_cache
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-# Update cache (can stay here as it doesn’t depend on unzip_client)
+# Update cache
 update_cache()
 
-# Remove the unzip_client instantiation here
+# Remove Buttons instantiation here
 # It will be handled in main.py instead
-
-# Buttons
-from .helpers_nexa.buttons import Unzipper_Buttons
-Buttons = Unzipper_Buttons()
